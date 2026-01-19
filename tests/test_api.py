bf171aa11d73415b51c37337fc7e5f56457b383c
LerouxMathis <mathisleroux32@gmail.com>
2026-01-19 16:30:48 +0100

    Initial DevSecOps projet8

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,4 +10,5 @@ def test_home_status_code():
 def test_home_response_content():
     client = app.test_client()
     response = client.get("/")
-    assert response.json == {"message": "Hello Ops"}
+    assert response.json == {"message": "API DevSecOps OK"}
+
